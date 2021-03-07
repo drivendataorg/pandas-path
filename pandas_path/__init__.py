3729a74e44f7219977ec461c3c691e6103692be3
Peter Bull <pjbull@gmail.com>
2021-03-06 19:37:29 -0600

    Add __version__ (#6)

--- pandas_path/__init__.py
+++ pandas_path/__init__.py
@@ -1,3 +1,13 @@
+import sys
+
 from .accessor import register_path_accessor  # noqa
 
 __all__ = [register_path_accessor]
+
+if sys.version_info[:2] >= (3, 8):
+    import importlib.metadata as importlib_metadata
+else:
+    import importlib_metadata
+
+
+__version__ = importlib_metadata.version(__name__.split(".", 1)[0])
